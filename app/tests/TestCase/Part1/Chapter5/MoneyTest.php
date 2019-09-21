160db66ae95fad347e6e2ba905e58e3e34381501
田村行生 <>
2019-09-21 15:01:06 +0900

    chapter 5 : copy chapter 4 to start

---
+++ app/tests/TestCase/Part1/Chapter5/MoneyTest.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme\Tests\TestCase\Part1\Chapter5;
+
+use PHPUnit\Framework\TestCase;
+use Acme\Part1\Chapter5\Dollar;
+
+class MoneyTest extends TestCase
+{
+    public function testMultiplication()
+    {
+        $five = new Dollar(5);
+
+        $this->assertTrue((new Dollar(10))->equals($five->times(2)));
+        $this->assertTrue((new Dollar(15))->equals($five->times(3)));
+    }
+
+    public function testEquality()
+    {
+        $this->assertTrue((new Dollar(5))->equals(new Dollar(5)));
+        $this->assertFalse((new Dollar(5))->equals(new Dollar(6)));
+    }
+}
