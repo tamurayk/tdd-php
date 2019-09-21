c2463578d48962b4bdac8897ecccb70cb8482352
田村行生 <>
2019-09-22 08:44:21 +0900

    chapter 8 : done

--- app/src/Part1/Chapter8/Money.php
+++ app/src/Part1/Chapter8/Money.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Acme\Part1\Chapter8;
 
-class Money{
+abstract class Money{
     /** @var  int */
     protected $amount;
 
@@ -16,4 +16,28 @@ class Money{
         return ($this->amount === $money->amount) &&
             (get_class($this) === get_class($money));
     }
+
+    /**
+     * @param int $amount
+     * @return Money
+     */
+    static function dollar(int $amount): Money
+    {
+        return new Dollar($amount);
+    }
+
+    /**
+     * @param int $amount
+     * @return Money
+     */
+    static function franc(int $amount): Money
+    {
+        return new Franc($amount);
+    }
+
+    /**
+     * @param int $multiplier
+     * @return Money
+     */
+    abstract function times(int $multiplier): Money;
 }
