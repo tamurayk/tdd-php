6530bf18afec0a289075176079f9a6ec3b84dddc
田村行生 <>
2019-09-23 20:43:25 +0900

    コメント追記

--- app/src/Part1/Chapter13/Sum.php
+++ app/src/Part1/Chapter13/Sum.php
@@ -21,10 +21,12 @@ class Sum implements Expression {
     }
 
     /**
+     * 為替レートを適用した結果を返す
+     *
      * @param string $to
      * @return Money
      */
-    public function reduce(string $to): Money
+    public function reduce(string $to): Money //reduce = 式を単純な形に変形(簡約)するという意味
     {
         $amount = $this->augend->amount() + $this->addend->amount();
         return new Money($amount, $to);
