8dd7ef689bab0ac129898665853382c11ba8a1b5
田村行生 <>
2019-10-02 12:55:51 +0900

    chapter 4 : copy chapter 3 to start

---
+++ app/src/Part1/Chapter4/Dollar.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme\Part1\Chapter4;
+
+class Dollar
+{
+    /**
+     * @var int
+     */
+    public $amount;
+
+    /**
+     * Dollar constructor.
+     * @param int $amount
+     */
+    public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @param int $multiplier
+     * @return Dollar
+     */
+    public function times(int $multiplier): Dollar
+    {
+        return new Dollar($this->amount * $multiplier);
+    }
+
+    /**
+     * @param Dollar $object
+     * @return bool
+     */
+    public function equals(Dollar $object): bool
+    {
+        $dollar = $object;
+
+        return $this->amount == $dollar->amount;
+    }
+}
