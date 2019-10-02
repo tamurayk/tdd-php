d4ab83ecc289ef5e7a67eddfa3782defdafdb139
田村行生 <>
2019-10-02 12:31:05 +0900

    chapter 2 : done

--- app/src/Part1/Chapter2/Dollar.php
+++ app/src/Part1/Chapter2/Dollar.php
@@ -21,9 +21,10 @@ class Dollar
 
     /**
      * @param int $multiplier
+     * @return Dollar
      */
-    public function times(int $multiplier): void
+    public function times(int $multiplier): Dollar
     {
-        $this->amount *= $multiplier;
+        return new Dollar($this->amount * $multiplier);
     }
 }
--- app/src/Part1/Chapter2/MoneyTest.php
+++ app/src/Part1/Chapter2/MoneyTest.php
@@ -10,7 +10,10 @@ class MoneyTest extends TestCase
     public function testMultiplication()
     {
         $five = new Dollar(5);
-        $five->times(2);
-        $this->assertSame(10, $five->amount);
+        $product = $five->times(2);
+        $this->assertSame(10, $product->amount);
+
+        $product = $five->times(3);
+        $this->assertSame(15, $product->amount);
     }
 }
