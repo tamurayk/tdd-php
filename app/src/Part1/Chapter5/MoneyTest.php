b221c2f2dee949b9e391a5917a2494d0a31c0427
田村行生 <>
2019-10-03 10:29:30 +0900

    chapter 5 : start

---
+++ app/src/Part1/Chapter5/MoneyTest.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme\Part1\Chapter5;
+
+use PHPUnit\Framework\TestCase;
+
+class MoneyTest extends TestCase
+{
+    public function testMultiplication()
+    {
+        $five = new Dollar(5);
+
+        $this->assertEquals(new Dollar(10), $five->times(2));
+        $this->assertEquals(new Dollar(15), $five->times(3));
+    }
+
+    public function testEquality()
+    {
+        $this->assertTrue((new Dollar(5))->equals(new Dollar(5)));
+        $this->assertFalse((new Dollar(5))->equals(new Dollar(6)));
+    }
+}
