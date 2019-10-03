ce0a5995239408ecfea014e12ec44f31ec31943f
田村行生 <>
2019-10-03 11:11:14 +0900

    chapter 8 : start

---
+++ app/src/Part1/Chapter8/Money.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme\Part1\Chapter8;
+
+class Money
+{
+    /** @var int */
+    protected $amount;
+
+
+    /**
+     * @param Money $object
+     * @return bool
+     */
+    public function equals(Money $object): bool
+    {
+        $money = $object;
+
+        return $this->amount == $money->amount
+            && get_class($this) == get_class($object);
+    }
+}
---
+++ app/src/Part1/Chapter8/MoneyTest.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme\Part1\Chapter8;
+
+use PHPUnit\Framework\TestCase;
+
+class MoneyTest extends TestCase
+{
+    public function testMultiplication()
+    {
+        $five = new Dollar(5);
+
+        $this->assertEquals(new Dollar(10), $five->times(2));
+        $this->assertEquals(new Dollar(15), $five->times(3));
+    }
+
+    public function testEquality()
+    {
+        $this->assertTrue((new Dollar(5))->equals(new Dollar(5)));
+        $this->assertFalse((new Dollar(5))->equals(new Dollar(6)));
+
+        $this->assertTrue((new Franc(5))->equals(new Franc(5)));
+        $this->assertFalse((new Franc(5))->equals(new Franc(6)));
+
+        $this->assertFalse((new Franc(5))->equals(new Dollar(5)));
+    }
+
+    public function testFrancMultiplication()
+    {
+        $five = new Franc(5);
+
+        $this->assertEquals(new Franc(10), $five->times(2));
+        $this->assertEquals(new Franc(15), $five->times(3));
+    }
+}
