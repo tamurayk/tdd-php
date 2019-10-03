826c531b5f3af854a88af70dd73cd3c908d69f96
田村行生 <>
2019-10-03 11:03:27 +0900

    chapter 7 : start

---
+++ app/src/Part1/Chapter7/Franc.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme\Part1\Chapter7;
+
+class Franc extends Money
+{
+    /**
+     * Franc constructor.
+     * @param int $amount
+     */
+    public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @param int $multiplier
+     * @return Franc
+     */
+    public function times(int $multiplier): Franc
+    {
+        return new Franc($this->amount * $multiplier);
+    }
+}
---
+++ app/src/Part1/Chapter7/Money.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme\Part1\Chapter7;
+
+class Money
+{
+    /** @var int */
+    protected $amount;
+
+
+    /**
+     * @param Money $object
+     * @return bool
+     */
+    public function equals(Money $object): bool
+    {
+        $money = $object;
+
+        return $this->amount == $money->amount;
+    }
+}
