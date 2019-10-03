074a4cdcf262e21c0402f025519cdc292b774ff6
田村行生 <>
2019-10-03 10:44:08 +0900

    chapter 6 : start

---
+++ app/src/Part1/Chapter6/Dollar.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme\Part1\Chapter6;
+
+class Dollar
+{
+    /**
+     * @var int
+     */
+    private $amount;
+
+    /**
+     * Dollar constructor.
+     * @param int $amount
+     */
+    public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @param int $multiplier
+     * @return Dollar
+     */
+    public function times(int $multiplier): Dollar
+    {
+        return new Dollar($this->amount * $multiplier);
+    }
+
+    /**
+     * @param Dollar $object
+     * @return bool
+     */
+    public function equals(Dollar $object): bool
+    {
+        $dollar = $object;
+
+        return $this->amount == $dollar->amount;
+    }
+}
