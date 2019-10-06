4aa186d6d56bc2b4028dee60dda8c7f9d4c7fec0
田村行生 <>
2019-10-07 07:23:36 +0900

    chapter 10 : start

---
+++ app/src/Part1/Chapter10/Money.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme\Part1\Chapter10;
+
+abstract class Money
+{
+    /** @var int */
+    protected $amount;
+
+    /** @var string */
+    protected $currency;
+
+    public function __construct(int $amount, string $currency)
+    {
+        $this->amount = $amount;
+        $this->currency = $currency;
+    }
+
+    /**
+     * @param int $multiplier
+     * @return Money
+     */
+    abstract function times(int $multiplier): Money;
+
+    public function currency(): string
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param Money $object
+     * @return bool
+     */
+    public function equals(Money $object): bool
+    {
+        $money = $object;
+
+        return $this->amount == $money->amount
+            && get_class($this) == get_class($object);
+    }
+
+    /**
+     * @param int $amount
+     * @return Money
+     *
+     * note:
+     *   Factory Method(= オブジェクトを作成するメソッド)
+     *
+     */
+    static function dollar(int $amount): Money
+    {
+        return new Dollar($amount, 'USD');
+    }
+
+    /**
+     * @param int $amount
+     * @return Money
+     *
+     * note:
+     *   Factory Method(= オブジェクトを作成するメソッド)
+     *
+     */
+    static function franc(int $amount): Money
+    {
+        return new Franc($amount, 'CHF');
+    }
+}
