c1812784323601bedf5d2c78fe9e8bba5d9f8bb9
田村行生 <>
2019-10-24 10:29:26 +0900

    Chapter 20 : done

--- app/src/Part2/Chapter20/xunit.py
+++ app/src/Part2/Chapter20/xunit.py
@@ -1,41 +1,35 @@
 class TestCase:
     def __init__(self, name):
         # Pluggable Selector パターン
-        #  why: インスタンス毎に異なる振る舞いをさせる
-        #  how: リフレクションで動的にメソッドの起動を行う
+        #  ends(目的): インスタンス毎に異なる振る舞いをさせる
+        #  means(手段): リフレクションで動的にメソッドの起動を行う
         self.name = name
     def setUp(self):
+        pass
+    def tearDown(self):
         pass
     def run(self):
         self.setUp()
         method = getattr(self, self.name) # Pluggable Selector パターン
         method() # Pluggable Selector パターン
+        self.tearDown()
 
 # 「テストが走ったかどうかを答えるテスト」のテスト対象クラス
 class WasRun(TestCase): # TestCaseクラスを継承
     def setUp(self):
-        self.wasRun = None # Python では None が null に相当。評価結果が 偽 となる。
-        self.wasSetUp = 1
+        # Log String (記録用文字列) パターン
+        #  ends: 正しい順序でメソッド呼び出しが行われていることをテストする
+        #  means: 記録用文字列を作り、メソッド呼び出しのたびにその文字列を追記
+        self.log = "setUp "
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 class TestCaseTest(TestCase): # TestCaseクラスを継承
-    def setUp(self):
-        # フィクスチャー
-        #  why:
-        #    - 複数のテストから使われる共通のオブジェクトを作る
-        #  how:
-        #    - テストメソッド内のローカル変数をインスタンス変数に引き上げる
-        #    - オーバーライドした setUP メソッドの中で初期化を行う
-        #  note:
-        #    - 各々のテストメソッドはそれぞれ別のインスタンで動いているので、依存関係は発生しない
-        self.test = WasRun("testMethod")
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log) # Log String パターン
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
