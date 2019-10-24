01683557e513301ea106d6c4c7d989de27302712
田村行生 <>
2019-10-24 10:43:58 +0900

    Chapter 16 : start

---
+++ app/src/Part1/Chapter16/Pair.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme\Part1\Chapter16;
+
+class Pair
+{
+    /** @var string */
+    private $from;
+
+    /** @var string */
+    private $to;
+
+    /**
+     * Pair constructor.
+     * @param string $from
+     * @param string $to
+     */
+    public function __construct(string $from, string $to)
+    {
+        $this->from = $from;
+        $this->to = $to;
+    }
+
+    /**
+     * @param Pair $pair
+     * @return bool
+     */
+    public function equals($pair): bool
+    {
+        return $this->from === $pair->from && $this->to === $pair->to;
+    }
+
+    /**
+     * @return int
+     */
+    public function hashCode(): int
+    {
+        return 0;
+    }
+}
