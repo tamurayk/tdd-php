d7bf841ebb5a678f9f41668fc45258d28cc5fa72
田村行生 <>
2019-10-24 11:41:39 +0900

    Chapter 16 : done

--- app/src/Part1/Chapter16/Expression.php
+++ app/src/Part1/Chapter16/Expression.php
@@ -6,6 +6,12 @@ namespace Acme\Part1\Chapter16;
 interface Expression
 {
     /**
+     * @param int $multiplier
+     * @return Expression
+     */
+    public function times(int $multiplier): Expression;
+
+    /**
      * @param Expression $addend
      * @return Expression
      */
@@ -17,10 +23,4 @@ interface Expression
      * @return Money
      */
     public function reduce(Bank $bank, string $to): Money;
-
-    /**
-     * @param int $multiplier
-     * @return Expression
-     */
-    public function times(int $multiplier): Expression;
 }
